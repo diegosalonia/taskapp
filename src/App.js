e09bb3a9bacec8df77db91ab366e4dfc5e56cbc1
diego martin Salonia <diegosalonia@gmail.com>
2021-05-28 19:31:56 -0300

    Sample Tasks Saved in Local Storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TaskRow } from "./componentes/TaskRow";
 import { TaskBanner } from "./componentes/TaskBanner";
 import { TaskCreator } from "./componentes/TaskCreator";
 import { VisibilityControl } from "./componentes/VisibilityControl";
-
 
 function App() {
   const [userName, setUserName] = useState("Diego");
@@ -15,29 +14,51 @@ function App() {
     { name: "Task Four", done: false },
   ]);
 
-  const [showCompleted, setShowCompleted] = useState(true)
+  const [showCompleted, setShowCompleted] = useState(true);
 
-  const createNewTask = taskName => {
-    if(!taskItems.find(t => t.name === taskName)) {
-      setTaskItems([...taskItems, {name: taskName, done: false}])
+  useEffect(() => {
+    let dataLocStor = localStorage.getItem("tasks");
+    if (dataLocStor != null) {
+      setTaskItems(JSON.parse(dataLocStor));
+    } else {
+      setUserName('Diego')
+      setTaskItems([
+        { name: "Task One Example", done: false },
+        { name: "Task Two Example", done: false },
+        { name: "Task Three Example", done: true },
+        { name: "Task Four Example", done: false }
+      ])
+      setShowCompleted(true);
     }
-  } 
-  const toggleTask = task => 
-    setTaskItems(taskItems.map(t => (t.name === task.name ? {...t, done: !t.done} : t)))
+  }, []);
 
-  const taskTableRows = (doneValue) => 
-  taskItems
-  .filter(task => task.done === doneValue)
-  .map(task => (
-      <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
-    ))
-  
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(taskItems));
+  }, [taskItems]);
+
+  const createNewTask = (taskName) => {
+    if (!taskItems.find((t) => t.name === taskName)) {
+      setTaskItems([...taskItems, { name: taskName, done: false }]);
+    }
+  };
+  const toggleTask = (task) =>
+    setTaskItems(
+      taskItems.map((t) => (t.name === task.name ? { ...t, done: !t.done } : t))
+    );
+
+  const taskTableRows = (doneValue) =>
+    taskItems
+      .filter((task) => task.done === doneValue)
+      .map((task) => (
+        <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
+      ));
+
   return (
     <div className="App">
       <TaskBanner userName={userName} taskItems={taskItems} />
-      
+
       <TaskCreator callback={createNewTask} />
-      <table className= "table table-striped table-dark table-bordered table-hover">
+      <table className="table table-striped table-dark table-bordered table-hover">
         <thead>
           <tr>
             <th>Description</th>
@@ -48,14 +69,13 @@ function App() {
       </table>
 
       <div className="bg-secondary-text-white text-center p-2">
-        <VisibilityControl 
-        description="Completed Task" 
-        isChecked={showCompleted}
-        callback={checked => setShowCompleted(checked)}
+        <VisibilityControl
+          description="Completed Task"
+          isChecked={showCompleted}
+          callback={(checked) => setShowCompleted(checked)}
         />
       </div>
-      {
-        showCompleted && (
+      {showCompleted && (
         <table className="table table-striped table-dark table-bordered table-hover">
           <thead>
             <tr>
@@ -63,13 +83,9 @@ function App() {
               <th>Done</th>
             </tr>
           </thead>
-          <tbody>
-            {taskTableRows(true)}
-          </tbody>
+          <tbody>{taskTableRows(true)}</tbody>
         </table>
-        )
-        
-      }
+      )}
     </div>
   );
 }
