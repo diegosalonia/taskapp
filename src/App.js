3d1e76729c7bb916e9aa73a21d5918815c4ef38f
diego martin Salonia <diegosalonia@gmail.com>
2021-05-28 18:53:38 -0300

    adding functionality

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import { TaskRow } from "./componentes/TaskRow";
 import { TaskBanner } from "./componentes/TaskBanner";
-
-
+import { TaskCreator } from "./componentes/TaskCreator";
 
 function App() {
   const [userName, setUserName] = useState("Diego");
@@ -14,6 +13,13 @@ function App() {
     { name: "Task Four", done: false },
   ]);
 
+  const [showCompleted, setShowCompleted] = useState(true)
+
+  const createNewTask = taskName => {
+    if(!taskItems.find(t => t.name === taskName)) {
+      setTaskItems([...taskItems, {name: taskName, done: false}])
+    }
+  } 
   const toggleTask = task => 
     setTaskItems(taskItems.map(t => (t.name === task.name ? {...t, done: !t.done} : t)))
 
@@ -24,6 +30,8 @@ function App() {
   return (
     <div className="App">
       <TaskBanner userName={userName} taskItems={taskItems} />
+      
+      <TaskCreator callback={createNewTask} />
       <table className= "table table-striped table-dark table-bordered table-hover">
         <thead>
           <tr>
